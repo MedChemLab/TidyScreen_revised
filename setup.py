fb892fdcd2fc169e2fc791d1a73ddf4888da2e8d
Alfredo <aquevedo@unc.edu.ar>
2024-11-01 15:20:53 -0300

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(name='tidyscreeb',
+      version='0.0.1',
+      description='A package to manage data associated to drug screening campaigns',
+      long_description=open('README.md').read().strip(),
+      author="MedChemLab staff",
+      author_email="aquevedo@unc.edu.ar",
+      url='https://github.com/MedChemLab/TidyScreen_revised',
+      license='GPLv3',
+      python_requires='>=3.10',
+      install_requires=[
+        'pandas==2.2.1',
+        'rdkit==2023.9.5',
+        'openbabel=3.1.1'
+        'sqlite=3.46.1'
+      ],
+      packages=find_packages(exclude=("utilities")),
+      keywords='drug-discovery machine-learning ersilia chembl',
+      classifiers=[
+          "Programming Language :: Python :: 3.10",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering :: Artificial Intelligence",
+      ],
+      include_package_data=True
+      )
